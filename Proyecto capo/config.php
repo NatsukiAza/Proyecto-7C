48a9a16c9aa7baf5e99f490b762c22cf0cbf59f9
Puchis <belpucas@gmail.com>
2022-06-09 15:00:09 -0300

    Se termino la subida de datos :rage2: :feelsgood: :goberserk:

--- Proyecto capo/config.php
+++ Proyecto capo/config.php
@@ -4,6 +4,9 @@
     $database = 'aromatic_data_base';
     $host = 'localhost';
 
+    $coneccion = mysqli_connect("localhost", "root", "", "aromatic_data_base");
+    mysqli_set_charset($coneccion, "utf8");
+
     try{
         $conn = new PDO("mysql:host=".$host.";dbname=".$database, $user, $pass);
         /* die("Conectado"); */
--- Proyecto capo/crearPublicacion.php
+++ Proyecto capo/crearPublicacion.php
@@ -10,4 +10,15 @@ $imagenes = $_POST["imagenes"];
 
 $insertar = "INSERT INTO publicaciones (descripcion, nombre, stock, imagenes, precio) VALUES ('$descripcion', '$nombre', '$stock', '$imagenes', '$precio')";
 
+$resultado = mysqli_query($coneccion, $insertar);
+if($resultado)
+{
+    echo "<script>alert('La publicacion se creo con exito');
+    window.location='/Tabla-datos-grid'</script>";
+}
+else 
+{
+   echo "<script>('No se pudo crear la publicacion'); window.history.go(-1);</script>";
+}
+
 ?>
