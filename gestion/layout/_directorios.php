ea3a7ef65396b7874d9c35c3867eb7e780de8104
NatsukiAza <santinoazarola@gmail.com>
2022-09-26 00:01:26 -0300

    shorty tiene

--- gestion/layout/_directorios.php
+++ gestion/layout/_directorios.php
@@ -1,10 +1,15 @@
-<div class="body-nav-bar">
 
-		<div class="adminb">
-		<h4 style="text-align: center; padding: 20px 0px 20px 0px;">Administrador</h4>
-		</div>
-	<a href="main.php">Inicio</a></li>
-	<a href="pedidos.php">Pedidos</a></li>
-	<a href="productos.php">Productos</a></li>
-	<a href="index.php">Salir</a></li>
+<label class="chelo" for="btn-menu">☰</label>
+<input type="checkbox" id="btn-menu">
+<div class="container-menu">
+	<div class="cont-menu">
+		<nav>
+			<a href="main.php">Inicio</a></li>
+			<a href="pedidos.php">Pedidos</a></li>
+			<a href="productos.php">Productos</a></li>
+			<a href="index.php">Salir</a></li>
+		</nav>
+		<label for="btn-menu">✖️</label>
+	</div>
 </div>
+
