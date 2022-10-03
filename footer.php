b4e1a369a0ee3879e48721c27dde82c9f6512e9d
NatsukiAza <santinoazarola@gmail.com>
2022-10-03 01:02:16 -0300

    DAY N NITE
    remodelacion completa del footer y cambio a circulos, izq y der aparte de ultimos toques a masv

---
+++ footer.php
@@ -0,0 +1,55 @@
+<footer class="info">
+      
+      <div class="infopag">
+        <a class="cereb" href="index.php">Aromatic</a>
+        <div class="redes">
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><img width="13px" src="img/face.png" alt="Facebook"></a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><img width="13px" src="img/twitter.png" alt="Twitter"></a>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><img width="13px" src="img/insta.png" alt="Instagram"></a>
+        </div>
+        <a href=""><img src="img/tel.png" alt="Telefono">+54 9 11 5506 8746</a>
+        <a href="https://www.google.com.ar/maps/place/Ramos+Mejía,+Provincia+de+Buenos+Aires/@-34.6538875,-58.5757019,14z/data=!3m1!4b1!4m5!3m4!1s0x95bcc7d93bab75a3:0xaafe140bc9dea3db!8m2!3d-34.6549073!4d-58.5536355"><img src="img/ubicacion.png" alt="Ubicacion">Ramos Mejia</a>
+      </div>
+
+      <div class="dividir"></div>
+
+      <div class="indice">
+
+      <div class="tienda">
+        <a href=""></a>
+      </div>
+
+      <div class="perfil">
+        <a href=""></a>
+      </div>
+
+      <div class="acerca">
+        <div class="a"></div>
+      </div>
+
+      <div class="mas">
+        <a href=""></a>
+      </div>
+
+      <div class="gragus">
+        <a href=""></a>
+      </div>
+
+      <div class="saph">
+        <a href=""></a>
+      </div>
+
+      <div class="disclaimer">
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
+      
+        <div class="copyright">
+            <h1>Copyright © 2022 Gragus Corp</h1>
+            <h2>Al usar Aromatic.com, usted conciente el monitoreo y almacenamiento de su interaccion con el sitio web, incluyendo interacciones con un vendedor, para su uso en personalizar y mejorar nuestros servicios. Nunca venderiamos tu información (Kappa). Mira nuestra <a href="">Politica de Privacidad</a> para mas detalles</h2>
+        </div>
+
+      </div>
+
+    </footer>
--- index.php
+++ index.php
@@ -7,14 +7,14 @@
   <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald:wght@400;700&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style1.css">
     <link rel="stylesheet" href="css/queries.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Inicio</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   </head>
   <body>
 
@@ -57,70 +57,35 @@
         </div>
       </div>
 
-  <div style="margin-top: 60%;">
-
-      <h2 class="titulo">¡Bienvenido a Aromatic!</h2>
-      <p class="subt">El aroma te transporta a lugares lejanos, a los seres queridos y amados,  a momentos especiales guardados en gratos recuerdos.
-        Se necesita sólo un instante para que todo lo que te rodea se vuelva encantador.</p>
-
-
     <div class="backg"></div> 
     <div class="cgrup">
     <div class="circulo" id="ciz"></div>
     <div class="imag2"><img src="img/fragancia.png" width="100%"></div> 
     </div>
-    <div class="card" id="izq" style="width: 18rem;">
-      <div class="card-body">
-        <h1 class="card-title fw-bolder">Fragancias</h1>
-        <h5 class="card-subtitle mb-2 fw-bold">Mira algunas de nuestras mejores fragancias</h5>
-        <hr>
-        <p id="espacio" class="card-text fw-lighter">Amour Fresa <a href="productos.html">Comprar</a></p>
-        <hr>
-        <p id="espacio" class="card-text fw-lighter">Amour Fresa <a href="productos.html">Comprar</a></p>
-        <hr>
-        <p id="espacio" class="card-text fw-lighter">Amour Fresa <a href="productos.html">Comprar</a></p>
-        <hr>
-        <p id="espacio" class="card-text fw-lighter">Amour Fresa <a href="productos.html">Comprar</a></p>
-        <hr>
-        <a href="#" class="card-link">Ver difusores</a>
-        <a href="#" class="card-link">Ver aromas </a>
-      </div>
+
+    <div class="future" id="izq">
+
+      <h1>¿Que es Aromatic?</h1>
+      <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ultricies odio. Mauris id pharetra lectus, id iaculis ante. Ut turpis dui, lobortis eget aliquam quis, cursus in dui. Aenean vehicula velit lorem, vitae imperdiet odio eleifend ac. Donec viverra posuere fringilla. Fusce consequat metus metus, at tempor leo suscipit vitae. Donec efficitur nisl mauris, quis viverra urna placerat non. Maecenas imperdiet urna vel justo ultrices accumsan. Aliquam tristique gravida eros id sollicitudin. Donec ut luctus nulla. Fusce imperdiet eleifend lacus congue blandit. In hac habitasse platea dictumst. Praesent orci lorem, posuere rhoncus blandit a, suscipit in est. Nam lacus nisl, blandit et tempor in, interdum sed erat.</h3>
+
     </div>
 
     <div class="cgrup">
     <div class="imag1"><img src="img/difusor.png" width="100%"></div> 
     <div class="circulo" id="cde"></div>
     </div>
-    <div class="card" id="der" style="width: 18rem;">
-      <div class="card-body">
-        <h1 class="card-title fw-bolder">Difusores</h1>
-        <h5 class="card-subtitle mb-2 fw-bold">Busca el difusor que mejor se ajuste a vos</h5>
-        <hr>
-        <p id="espacio" class="card-text fw-lighter">Amour Fresa <a href="productos.html">Comprar</a></p>
-        <hr>
-        <p id="espacio" class="card-text fw-lighter">Amour Fresa <a href="productos.html">Comprar</a></p>
-        <hr>
-        <p id="espacio" class="card-text fw-lighter">Amour Fresa <a href="productos.html">Comprar</a></p>
-        <hr>
-        <p id="espacio" class="card-text fw-lighter">Amour Fresa <a href="productos.html">Comprar</a></p>
-        <hr>
-        <a href="#" class="card-link">Ver difusores</a>
-        <a href="#" class="card-link">Ver aromas </a>
-      </div>
+
+    <div class="future" id="der">
+
+      <h1>Envios</h1>
+      <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ultricies odio. Mauris id pharetra lectus, id iaculis ante. Ut turpis dui, lobortis eget aliquam quis, cursus in dui. Aenean vehicula velit lorem, vitae imperdiet odio eleifend ac. Donec viverra posuere fringilla. Fusce consequat metus metus, at tempor leo suscipit vitae. Donec efficitur nisl mauris, quis viverra urna placerat non. Maecenas imperdiet urna vel justo ultrices accumsan. Aliquam tristique gravida eros id sollicitudin. Donec ut luctus nulla. Fusce imperdiet eleifend lacus congue blandit. In hac habitasse platea dictumst. Praesent orci lorem, posuere rhoncus blandit a, suscipit in est. Nam lacus nisl, blandit et tempor in, interdum sed erat.</h3>
+
     </div>
 
-    <a class="bt" href="moger/index.php">¡Empeza a mirar nuestros productos ahora!</a>
+    <div style="margin-top: 20%;"> </div>
 
-    <div style="margin-top: 30%;"> </div>
-    <footer style="text-align: center;" class="info">
-      <p class="licencia">El contenido esta disponible bajo la licencia de Gragus Corp a menos que se indique lo contrario</p>
-      <section class="buttons">
-      <a href="#" class="fa fa-facebook"></a>
-      <a href="#" class="fa fa-twitter"></a>
-    </section>
-      <p style="margin-bottom: 0;">+54 9 11 5506 8746</p>
-    </footer>
-</div>
+    <?php include("footer.php"); ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
 </html>
