37da8cb530c69b8e5d93ec256812fb0f6f2f1b9f
NatsukiAza <santinoazarola@gmail.com>
2022-10-09 00:18:59 -0300

    login nashei, perfil nashei

--- ChatTReal/vistas/login.php
+++ ChatTReal/vistas/login.php
@@ -8,9 +8,9 @@
   <!DOCTYPE html>
   <html lang="en">
   <head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald:wght@400;700&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&family=Montserrat&family=Oswald:wght@400;700&family=Quicksand:wght@300;400;500;700&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../../css/queries.css">
     <link rel="stylesheet" href="../../css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
@@ -23,19 +23,22 @@
   
 <body>
 
+<p>Adquirí los productos desde la comodidad de tu hogar y aromatiza todos tus espacios con las mejores fragancias.</p>
+
   <div class="c">
   <div class="cajal">
+    <a class="volver" href="../../index.php"><img width="100%"src="../../img/flecha.png" alt="Volver"></a>
     <div class="cambiar">
     <a class="current" href="login.php">Inicia Sesion</a>
     <h1>o</h1>
     <a href="index.php">Registrate</a>
     </div>
 
-      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+      <form class="log" action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error"></div>
         <div class="datos">
           <label>E-MAIL</label>
-          <input type="text" name="email" placeholder="Introduce tu correo electrónico" required>
+          <input type="text" name="email" placeholder="Ingresa tu correo electrónico" required>
         </div>
         <div class="datos">
           <label>CONTRASEÑA</label>
@@ -43,7 +46,7 @@
         </div>
         <div class="b">
           <input type="submit" name="submit" value="Iniciar Sesion">
-          <a href="index.php">Todavia no tengo una cuenta</a>
+          <a id="esp" href="index.php">Todavia no tengo una cuenta</a>
         </div>
       </form>
     </div>
