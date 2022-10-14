9275691c337f7337c262ca749275793d6135aa2f
Puchis <belpucas@gmail.com>
2022-10-14 17:59:30 -0300

    you can't do that yugi
    fuck you

--- ChatTReal/vistas/index.php
+++ ChatTReal/vistas/index.php
@@ -7,9 +7,78 @@
   <!DOCTYPE html>
   <html lang="en">
   <head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&family=Montserrat&family=Oswald:wght@400;700&family=Quicksand:wght@300;400;500;700&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald:wght@400;700&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/queries.css">
+    <link rel="stylesheet" href="../../css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Aromatic - Inicio de Sesion</title>
+  </head>
+<body>
+
+  <?php include("navbar4.php"); ?>
+
+  <div class="wrapper">
+    <section class="form signup">
+      <header>Chat en tiempo real</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="name-details">
+          <div class="field input">
+            <label>Nombre</label>
+            <input type="text" name="fname" placeholder="Nombre" required>
+          </div>
+          <div class="field input">
+            <label>Apellido</label>
+            <input type="text" name="lname" placeholder="Apellido" required>
+          </div>
+        </div>
+        <div class="field input">
+          <label>Dirección de correo electrónico</label>
+          <input type="text" name="email" placeholder="Introduce tu correo electrónico" required>
+        </div>
+        <div class="field input">
+          <label>Contraseña</label>
+          <input type="password" name="password" placeholder="Introduzca nueva contraseña" required>
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field image">
+          <label>Foto</label>
+          <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Continuar chateando">
+        </div>
+      </form>
+      <div class="link">Ya te inscribiste? <a href="login.php">Inicia sesión ahora</a></div>
+    </section>
+  </div>
+
+	<?php include("footer4.php"); ?>
+
+  <script src="../js/pass-show-hide.js"></script>
+  <script src="../js/signup.js"></script>
+
+</body>
+</html>
+
+<!-- <?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: usuarios.php");
+  }
+?>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&family=Montserrat&family=Oswald:wght@400;700&family=Quicksand:wght@300;400;500;700&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../../css/queries.css">
     <link rel="stylesheet" href="../../css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
@@ -67,3 +136,4 @@
 
 </body>
 </html>
+ -->
--- ChatTReal/vistas/login.php
+++ ChatTReal/vistas/login.php
@@ -8,6 +8,57 @@
   <!DOCTYPE html>
   <html lang="en">
   <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald:wght@400;700&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/style1.css">
+    <link rel="stylesheet" href="../../css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Aromatic - Inicio de Sesion</title>
+  </head>
+  
+<body>
+
+  <?php include("navbar4.php"); ?>
+
+  <div class="wrapper">
+    <section class="form login">
+      <header>Chat en tiempo real</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="field input">
+          <label>Dirección de correo electrónico</label>
+          <input type="text" name="email" placeholder="Introduce tu correo electrónico" required>
+        </div>
+        <div class="field input">
+          <label>Contraseña</label>
+          <input type="password" name="password" placeholder="Ingresa tu contraseña" required>
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Continuar chateando">
+        </div>
+      </form>
+      <div class="link">Aún no te has registrado? <a href="index.php">Regístrate ahora</a></div>
+    </section>
+  </div>
+  
+	<?php include("footer4.php"); ?>
+
+  <script src="../js/pass-show-hide.js"></script>
+  <script src="../js/login.js"></script>
+
+</body>
+</html>
+
+<!-- 
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&family=Montserrat&family=Oswald:wght@400;700&family=Quicksand:wght@300;400;500;700&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
@@ -58,3 +109,4 @@
 
 </body>
 </html>
+-->
