71b82a3b991b3f89ade1fa8cfa04f35b507bb1c1
Puchis <70609993+Puchis@users.noreply.github.com>
2022-10-19 17:32:36 -0300

    And Pucho said: "Let there be login fachero funcional!"
    And so it was (ma o meno)

--- ChatTReal/vistas/login.php
+++ ChatTReal/vistas/login.php
@@ -5,57 +5,7 @@
   }
 ?>
 
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald:wght@400;700&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../../css/style1.css">
-    <link rel="stylesheet" href="../../css/login.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Aromatic - Inicio de Sesion</title>
-  </head>
-  
-<body>
 
-  <?php include("navbar4.php"); ?>
-
-  <div class="wrapper">
-    <section class="form login">
-      <header>Chat en tiempo real</header>
-      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
-        <div class="error-text"></div>
-        <div class="field input">
-          <label>Dirección de correo electrónico</label>
-          <input type="text" name="email" placeholder="Introduce tu correo electrónico" required>
-        </div>
-        <div class="field input">
-          <label>Contraseña</label>
-          <input type="password" name="password" placeholder="Ingresa tu contraseña" required>
-          <i class="fas fa-eye"></i>
-        </div>
-        <div class="field button">
-          <input type="submit" name="submit" value="Continuar chateando">
-        </div>
-      </form>
-      <div class="link">Aún no te has registrado? <a href="index.php">Regístrate ahora</a></div>
-    </section>
-  </div>
-  
-	<?php include("footer4.php"); ?>
-
-  <script src="../js/pass-show-hide.js"></script>
-  <script src="../js/login.js"></script>
-
-</body>
-</html>
-
-<!-- 
   <!DOCTYPE html>
   <html lang="en">
   <head>
@@ -77,30 +27,34 @@
 <p>Adquirí los productos desde la comodidad de tu hogar y aromatiza todos tus espacios con las mejores fragancias.</p>
 
   <div class="c">
-  <div class="cajal">
-    <a class="volver" href="../../index.php"><img width="100%"src="../../img/flecha.png" alt="Volver"></a>
-    <div class="cambiar">
-    <a class="current" href="login.php">Inicia Sesion</a>
-    <h1>o</h1>
-    <a href="index.php">Registrate</a>
-    </div>
+    <div class="wrapper" id="cajal">
+      <section class="form login">
+      <a class="volver" href="../../index.php"><img width="100%"src="../../img/flecha.png" alt="Volver"></a>
+        <div class="cambiar">
+        <a class="current" href="login.php">Inicia Sesion</a>
+        <h1>o</h1>
+        <a href="index.php">Registrate</a>
+        </div>
 
-      <form class="log" action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
-        <div class="error"></div>
-        <div class="datos">
-          <label>E-MAIL</label>
-          <input type="text" name="email" placeholder="Ingresa tu correo electrónico" required>
-        </div>
-        <div class="datos">
-          <label>CONTRASEÑA</label>
-          <input type="password" name="password" placeholder="Ingresa tu contraseña" required>
-        </div>
-        <div class="b">
-          <input type="submit" name="submit" value="Iniciar Sesion">
-          <a id="esp" href="index.php">Todavia no tengo una cuenta</a>
-        </div>
-      </form>
+        <form class="log" action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+          <div class="error-text" id="error"></div>
+            <div class="field input" id="datos">
+            <label>E-MAIL</label>
+            <input type="text" name="email" placeholder="Ingresa tu correo electrónico" required>
+            </div>
+
+            <div class="field input" id="datos">
+            <label>CONTRASEÑA</label>
+            <input type="password" name="password" placeholder="Ingresa tu contraseña" required>
+            </div>
+
+            <div class="field button" id="b">
+            <input type="submit" name="submit" value="Iniciar Sesion">
+            <a id="esp" href="index.php">Todavia no tengo una cuenta</a>
+            </div>
+        </form>
     </div>
+    </section>
   </div>
   </div>
   
@@ -109,4 +63,3 @@
 
 </body>
 </html>
--->
