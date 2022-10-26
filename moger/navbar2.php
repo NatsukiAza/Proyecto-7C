f08389b4d480c4f7235010f03ceb0dc104b5de0d
Jaime Howard <70484215+jaimehoward@users.noreply.github.com>
2022-10-26 14:48:59 -0300

    Navbar potentes
    Super potente los Navbar

--- moger/navbar2.php
+++ moger/navbar2.php
@@ -1,26 +1,9 @@
-<div class="esq">
-    
-    <a class="cereb" href="../index.php">Aromatic</a>
-
-    <div class="braz">
-        <a href="index.php">Tienda</a>
-        <a href="pedido.php">Pedidos</a>
-        <a href="historial.php">Historial</a>
-        <a href="../ChatTReal/vistas/index.php">Chats</a>
-    </div>
-    
-    <div class="pier">
-
-        <a href="carrito.php"><img height="30%" src="../img/carrito.png" alt="Carrito"></a>
-        <a href="perfil.php"><img height="30%" src="../img/usericon2.png" alt="Usuario"></a>
-
-    </div>
-</div>
-
-
-<div class="pata">
-
-    <input type="search" name="barrab" class="barrab">
-    <button type="submit" class="lupita"> <img width="170%" src="../img/lupa.png" alt="Buscar"> </button>
-
-</div>
+<?php 
+    $unique_id=$_SESSION['unique_id'];
+    if(336153552==$unique_id){  
+        include("navbaradmin2.php");
+    }
+    else{
+        include("navbarusu2.php");
+    }
+  ?>
