c2ee8dd4860dede8d066599d07c1f64ff508775b
NatsukiAza <santinoazarola@gmail.com>
2022-10-28 17:03:53 -0300

    :+1:

--- gestion/main.php
+++ gestion/main.php
@@ -54,19 +54,38 @@
 
   			</div>
 
-			<div id="c">
-				<div id="p">
-					<h5 class="card-title">Ventas Hechas</h5>
-    				<h6 class="card-subtitle mb-2">Ejemplo</h6>
-    				<p class="card-text">Ejemplo</p>
-					<p class="card-text">Ejemplo</p>
-					<div class="abajo">
-						<span><a href="historial.php" class="card-link">Ver mas</a></span>
-					</div>
-				</div>
-			</div>
+			<div id="c5">
+				<h1>Productos con poco stock</h1>
+
+    			<table id="tini">
+				<thead>
+					<tr>
+						<th>Nombre</th>
+						<th>Stock</th>
+					</tr>
+				</thead>	
+				<tbody>
+				<?php
+					$i = 0;
+					
+						$sql="SELECT * from producto where prodst<30";
+						$resultado=mysqli_query($con,$sql);
+						for ($i=0;$i<3;$i++)	 {
+						if ($row=mysqli_fetch_array($resultado)) {
+							echo 
+					'<tr>
+						<td class="animals">'.$row['nompro'].'</td>
+						<td class="sendra">'.$row['prodst'].'</td>	
+						</td>
+					</tr>';
+						}}	
+					?>
+				</tbody>
+				</table>
 
 			</div>
+			</div>
+
 
 			<div id="s">
 
@@ -103,38 +122,3 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
-
-						<!--
-			<div id="i">
-				<h5 class="card-title">Productos con poco stock</h5>
-    			<table id="ini">
-				<thead>
-					<tr>
-						<th style="text-align: center; width: 7%;">Código</th>
-						<th style="text-align: center; width: 15%">Nombre</th>
-						<th style="text-align: center; width: 4%">Stock</th>
-					</tr>
-				</thead>				
-				<tbody>
-				<?php
-					$i = 0;
-					
-						$sql="SELECT * from producto where prodst<30";
-						$resultado=mysqli_query($con,$sql);
-						for ($i=0;$i<3;$i++)	 {
-						if ($row=mysqli_fetch_array($resultado)) {
-							echo 
-					'<tr>
-						<td class="gorod"; style="text-align: center;" id="rosa">'.$row['codpro'].'</td>
-						<td class="animals"; style="padding-left: 10px">'.$row['nompro'].'</td>
-						<td class="sendra"; style="padding-left: 10px">'.$row['prodst'].'</td>	
-						</td>
-					</tr>';
-						}}
-						
-					?>
-				</tbody>
-				</table>
-			</div>
-
-			-->
