31bd68fd396bcbb56f3ab0f358f4beeed99a6aff
NatsukiAza <santinoazarola@gmail.com>
2022-10-30 15:10:13 -0300

    gestion nav

--- gestion/layout/_directorios.php
+++ gestion/layout/_directorios.php
@@ -1,17 +1,9 @@
 
-<label class="chelo" for="btn-menu">☰</label>
-<input type="checkbox" id="btn-menu">
-<div class="container-menu">
-	<div class="cont-menu">
-		<nav>
-			<a href="main.php">Inicio</a></li>
-			<a href="pedidos.php">Pedidos</a></li>
-			<a href="productos.php">Productos</a></li>
-			<a href="historial.php">Historial</a></li>
-			<a href="usuarios.php">Usuarios</a></li>
-			<a href="index.php">Salir</a></li>
-		</nav>
-		<label class="equis" for="btn-menu">X</label>
-	</div>
+<div id="anav">
+	<a href="main.php">Inicio</a></li>
+	<a href="pedidos.php">Pedidos</a></li>
+	<a href="productos.php">Productos</a></li>
+	<a href="historial.php">Historial</a></li>
+	<a href="usuarios.php">Usuarios</a></li>
+	<a href="index.php">Salir</a></li>
 </div>
-
