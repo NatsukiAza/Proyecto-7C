0a2360827a7c7bfdee3b7a03c621e94f57f15980
anashei <santinoazarola@gmail.com>
2022-10-31 15:00:20 -0300

    nashei

--- gestion/main.php
+++ gestion/main.php
@@ -12,6 +12,8 @@
 	<link rel="stylesheet" type="text/css" href="../css/index2.css">
     <link rel="stylesheet" type="text/css" href="../css/queries.css">
 	<link rel="stylesheet" href="../css/chat.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
@@ -160,29 +162,26 @@
 
 			<div id="c4"> 
 			
-<div class="wrapperr">
-  <section class="users">
-	<header>
-	  <div class="content">
-		<?php 
-		  $sql = mysqli_query($con, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
-		  if(mysqli_num_rows($sql) > 0){
-			$row = mysqli_fetch_assoc($sql);
-		  }
-		?>
-	<div class="search">
-	  <span class="text">Seleccione un usuario para iniciar el chat</span>
-	  <input type="text" placeholder="Ingrese el nombre para buscar ...">
-	  <button><i class="fas fa-search"></i></button>
+				<div class="wrapperr">
+  					<section class="users">
+						<?php 
+						$sql = mysqli_query($con, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+		 				if(mysqli_num_rows($sql) > 0){
+						$row = mysqli_fetch_assoc($sql);
+						}
+						?>
+						<div class="search">
+	  						<span class="text">Seleccione un usuario para iniciar el chat</span>
+	 						<input type="text" placeholder="Ingrese el nombre para buscar ...">
+	  						<button><i class="fas fa-search"></i></button>
+						</div>
+						<div class="users-list">
+						</div>
+  					</section>
+				</div>
+			</div>
+		</div>
 	</div>
-	<div class="users-list">
-
-	</div>
-  </section>
-</div>
-</div>
-</div>
-</div>
 
 	<script src="usuarios.js"></script>
 	<script type="text/javascript" src="../ChatTReal/js/jquery-3.4.1.min.js"></script>
